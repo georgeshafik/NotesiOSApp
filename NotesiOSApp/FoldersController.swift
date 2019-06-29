ae33143d4d9081e5955b20e643769811a42c9cb4
George Shafik <george@gsss.net>
2019-06-30 08:38:41 +1000

    first commit

---
+++ NotesiOSApp/FoldersController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NotesiOSApp
+//
+//  Created by George Shafik on 30/6/19.
+//  Copyright © 2019 George Shafik. All rights reserved.
+//
+
+import UIKit
+
+class FoldersController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
