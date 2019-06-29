25cd311f2e351a6041916922f60b87353c2355c4
George Shafik <george@gsss.net>
2019-06-30 08:45:57 +1000

    Add Navigation Controller. Closed #1.

--- NotesiOSApp/AppDelegate.swift
+++ NotesiOSApp/AppDelegate.swift
@@ -16,9 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        let navController = UINavigationController(rootViewController: FoldersController())
+        
         window = UIWindow()
         window?.makeKeyAndVisible()
-        window?.rootViewController = FoldersController()
+        window?.rootViewController = navController
         
         return true
     }
