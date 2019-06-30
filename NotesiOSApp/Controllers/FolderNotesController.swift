c440bdf00cf980be235f200763b32ab7471d99f6
George Shafik <george@gsss.net>
2019-06-30 10:25:29 +1000

    Setup a TableView for the new FolderNotesController Removed code from FolderNotesController didSelectRowAt Statically displaying 5 rows. Closed #5

--- NotesiOSApp/Controllers/FolderNotesController.swift
+++ NotesiOSApp/Controllers/FolderNotesController.swift
@@ -9,9 +9,35 @@
 import UIKit
 
 class FolderNotesController: UITableViewController {
+    
+    fileprivate let CELL_ID:String = "CELL_ID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = "Folder Notes"
+        
+        setupTableView()
+    }
+    
+    fileprivate func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: CELL_ID)
     }
 }
+
+extension FolderNotesController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath)
+        cell.textLabel?.text = "here's a note"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+
--- NotesiOSApp/Controllers/FoldersController.swift
+++ NotesiOSApp/Controllers/FoldersController.swift
@@ -8,25 +8,6 @@
 
 import UIKit
 
-extension FoldersController {
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath)
-        cell.textLabel?.text = "here's a note folder"
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let folderNotesController = FolderNotesController()
-        
-        // push it onto the stack
-        navigationController?.pushViewController(folderNotesController, animated: true)
-    }
-}
 
 // UITableViewController contains UITableViewSource and UITableViewDelegate
 class FoldersController: UITableViewController {
@@ -47,6 +28,28 @@ class FoldersController: UITableViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: CELL_ID)
     }
 
-
 }
+
+extension FoldersController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath)
+        cell.textLabel?.text = "here's a note folder"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let folderNotesController = FolderNotesController()
+        
+        // push it onto the stack
+        navigationController?.pushViewController(folderNotesController, animated: true)
+    }
+}
+
+
+
 
