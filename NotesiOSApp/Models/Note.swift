1994f08b5826d9e581295ce91f27b2fb5ae6bc36
George Shafik <george@gsss.net>
2019-07-01 22:17:52 +1000

    Create a NoteFolder Struct and Note Struct. Closed #18.

---
+++ NotesiOSApp/Models/Note.swift
@@ -0,0 +1,15 @@
+//
+//  Note.swift
+//  NotesiOSApp
+//
+//  Created by George Shafik on 1/7/19.
+//  Copyright © 2019 George Shafik. All rights reserved.
+//
+
+import Foundation
+
+struct Note {
+    var title:String
+    var date:Date
+    var text:String
+}
---
+++ NotesiOSApp/Models/NoteFolder.swift
@@ -0,0 +1,16 @@
+//
+//  NoteFolder.swift
+//  NotesiOSApp
+//
+//  Created by George Shafik on 1/7/19.
+//  Copyright © 2019 George Shafik. All rights reserved.
+//
+
+import Foundation
+
+struct NoteFolder {
+    var title:String;
+    var notes:[Note]
+}
+
+
